4a68633656d8d73919295c5a184f82fe1afc1564
Chelsea Isaacson <chelseaisaacson@Chelseas-MacBook-Air.local>
2020-04-06 15:51:00 -0700

    finished slices and started on comprehensions

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -10,8 +10,7 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
-
+y = [num for num in range(6) if num != 0]
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
